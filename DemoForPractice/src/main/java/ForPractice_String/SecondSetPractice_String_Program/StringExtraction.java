438f1bf31cf507c0de960b3dc9059882251f76b2
Welcome <Welcome@DESKTOP-FJSIPL4>
2023-08-30 20:05:43 +0530

    Latest Commit

--- DemoForPractice/src/main/java/ForPractice_String/SecondSetPractice_String_Program/StringExtraction.java
+++ DemoForPractice/src/main/java/ForPractice_String/SecondSetPractice_String_Program/StringExtraction.java
@@ -20,6 +20,28 @@ public class StringExtraction {
 		System.out.println("number_s1 : "+number_s1);
 		System.out.println("alphabet_s2 : "+alphabet_s2);
 		System.out.println("number_s2 : "+number_s2);
+		
+		String s3 ="welco@#m734e 3747to home12334$%^";
+				 
+				// String s4[] =s3.split(" ");
+		        //Remove Space from String
+		        String s4= s3.replaceAll("\\s", "");
+				 
+				 System.out.println(s4);
+				 System.out.println(s4.length());
+				 
+				 //Print only alphabet
+				 System.out.println(s4.replaceAll("[^a-z]", ""));
+				 //Print only Numeric
+				 System.out.println(s4.replaceAll("[^0-9]", ""));
+				 //Print only Special Character
+				 System.out.println(s4.replaceAll("[a-z0-9]", ""));
+				 
+				 //Remove Special Character from String
+				 System.out.println(s4.replaceAll("\\W", ""));
+				 //Remove alphabet
+				 System.out.println(s4.replaceAll("[a-zA-Z]", ""));
+				 //Remove Numeric
+				 System.out.println(s4.replaceAll("[0-9]", ""));				 
 	}
-
 }
